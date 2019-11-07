2acaf0b0a791269ccb8bf7a78a60ccfabde0ff19
zhangshaoqing <2107652428@qq.com>
2019-11-07 20:12:25 +0800

    2019-11-7-20:12

--- OCMExmaple/Extension/NSView+SQ.m
+++ OCMExmaple/Extension/NSView+SQ.m
@@ -19,4 +19,11 @@
     self.layer.cornerRadius = cornerRadio;
 }
 
+- (void)addCorner:(CGFloat)cornerRadio borderW:(CGFloat)borderWith borderColor:(NSColor *)borderColor{
+    self.wantsLayer = YES;
+    self.layer.masksToBounds = YES;
+    self.layer.cornerRadius = cornerRadio;
+    self.layer.borderWidth = borderWith;
+    self.layer.borderColor = borderColor.CGColor;
+}
 @end
